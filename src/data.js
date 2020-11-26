d8c598f1421a5b218c4a933f501fd95ac066ba66
RoSapia <roberta.sapia@hotmail.com>
2020-11-25 22:11:40 -0300

    adicionando função de buscar pokemon a app

--- src/data.js
+++ src/data.js
@@ -1,9 +1,19 @@
-// estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+/*filterData(data, condition): esta função receberia os dados e nos retornaria os que cumprem com a condição.
+sortData(data, sortBy, sortOrder): esta função recebe três parâmetros. O primeiro, data, nos entrega os dados. O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, indica se quer ordenar de maneira crescente ou decrescente.
+computeStats(data): essa função nos permite fazer cálculos estatísticos básicos para serem exibidos de acordo com o que os dados permitem.
+*/
+const pokemon = {
+   pesquisaPokemon(data, nomePokemon) {
+     console.log(nomePokemon)
+    let listaPokemon = data.pokemon
+    console.log(listaPokemon)
+    let busca = listaPokemon.find(i => i.name == nomePokemon)
+    console.log(busca)
+    return (busca)
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+  }
+
+}
+
+export default pokemon
