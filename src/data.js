94086f74e661c83522ae0ec50d74ba85874517b5
RoSapia <roberta.sapia@hotmail.com>
2020-12-02 13:59:14 -0300

    mostrando pokemons na tela

--- src/data.js
+++ src/data.js
@@ -1,19 +1,20 @@
-
 /*filterData(data, condition): esta função receberia os dados e nos retornaria os que cumprem com a condição.
 sortData(data, sortBy, sortOrder): esta função recebe três parâmetros. O primeiro, data, nos entrega os dados. O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, indica se quer ordenar de maneira crescente ou decrescente.
 computeStats(data): essa função nos permite fazer cálculos estatísticos básicos para serem exibidos de acordo com o que os dados permitem.
 */
+
 const pokemon = {
-   pesquisaPokemon(data, nomePokemon) {
-     console.log(nomePokemon)
-    let listaPokemon = data.pokemon
-    console.log(listaPokemon)
-    let busca = listaPokemon.find(i => i.name == nomePokemon)
-    console.log(busca)
+  pesquisaPokemon(data, nomePokemon) {
+    const listaPokemon = data.pokemon
+    let busca = listaPokemon.filter(p => p.name == nomePokemon)
     return (busca)
-
+  },
+  filtrarLendarios(data) {
+    const listaPokemon = data.pokemon
+    const lendarios = listaPokemon.filter(p => p['pokemon-rarity'] === 'legendary')
+    console.log(lendarios)
+    return (lendarios)
   }
-
 }
 
 export default pokemon
